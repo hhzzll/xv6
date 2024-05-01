ea93b2f9db7d60bc06a9c870e61fb49ad8f307e9
hzl <2283347410@qq.com>
2024-05-01 19:01:44 +0800

    complete lab6-multithreading

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -8,6 +8,8 @@
 #define NBUCKET 5
 #define NKEYS 100000
 
+pthread_mutex_t mtxes[NBUCKET];
+
 struct entry {
   int key;
   int value;
@@ -41,6 +43,7 @@ void put(int key, int value)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&mtxes[i]);
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -54,7 +57,7 @@ void put(int key, int value)
     // the new is new.
     insert(key, value, &table[i], table[i]);
   }
-
+  pthread_mutex_unlock(&mtxes[i]);
 }
 
 static struct entry*
@@ -117,6 +120,9 @@ main(int argc, char *argv[])
   for (int i = 0; i < NKEYS; i++) {
     keys[i] = random();
   }
+
+  for (int i = 0; i < NBUCKET; i++)
+    assert(pthread_mutex_init(&mtxes[i], NULL) == 0);
 
   //
   // first the puts
